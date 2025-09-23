35ad1b16c7c93b2384a67c8c920a315cdcdd9e48
Darshan Takarkhede <100999462+NeoAnthem@users.noreply.github.com>
2025-09-23 19:45:45 +0530

    Add files via upload

---
+++ JavaScript/Day06/arr1.js
@@ -0,0 +1,5 @@
+let arr1 = [5, 1, 7, 9, 3];
+
+for (let i = 0; i < arr1.length; i++) {
+    console.log("Element at position = ", i, " is ", arr1[i]);
+}
---
+++ JavaScript/Day06/arr2.js
@@ -0,0 +1,14 @@
+let arr1 = [5, 1, 7, 9, 3];
+/*
+for of loop / Enhance for loop
+1. Purpose of this loop is to iterate over values of iterable object
+2. Iterate over string, arrays, maps, or values of sets
+*/
+
+for (let element of arr1) {
+    console.log("Element  is ", element);
+}
+
+for (let char of "SPRK Tech") {
+    console.log(char);
+}
