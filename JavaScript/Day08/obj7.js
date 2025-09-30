213c875e39ccb8dac15eb7be8eecbef12e770f7d
Darshan Takarkhede <100999462+NeoAnthem@users.noreply.github.com>
2025-09-30 19:14:40 +0530

    Update obj7.js

--- JavaScript/Day08/obj7.js
+++ JavaScript/Day08/obj7.js
@@ -12,6 +12,14 @@ console.log("Random = ", random);
 randomBetween = random + 21;
 console.log("Random [21 - 25]= ", randomBetween);
 
-// 1 to 100
-// 30 to 50
+// WAP to generate a number between 1 to 100
+
+let num1to100 = Math.floor(Math.random() * 100) + 1;
+console.log("Random [1 - 100] = ", num1to100);
+
+// WAP to generate a number between 30 to 50
+
+let num30to50 = Math.floor(Math.random() * 21) + 30;
+console.log("Random [30 - 50] = ", num30to50);
+
 // Guess Number Game -> prompt (maximum)
